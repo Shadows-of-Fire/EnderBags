3ca8a5916e1506956e6ca0a8dc4d33564574757e
Shadows_of_Fire <Bward7864@gmail.com>
2019-03-18 23:36:07 -0400

    Add recipe unlocks

--- src/main/java/gigabit101/EnderBags/EnderBags.java
+++ src/main/java/gigabit101/EnderBags/EnderBags.java
@@ -7,16 +7,20 @@ import gigabit101.EnderBags.config.EnderBagConfig;
 import gigabit101.EnderBags.init.ModRegistry;
 import gigabit101.EnderBags.init.RecipeColour;
 import net.minecraft.block.Block;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.EnumDyeColor;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent.ItemCraftedEvent;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -44,6 +48,7 @@ public class EnderBags {
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(EnderBagConfig::onLoad);
 		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.GUIFACTORY, () -> GuiHandler::getClientGuiElement);
 		ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, EnderBagConfig.SPEC);
+		MinecraftForge.EVENT_BUS.register(this);
 	}
 
 	@SubscribeEvent
@@ -51,5 +56,21 @@ public class EnderBags {
 		RECIPES.addRecipe(new RecipeColour(null));
 		Block ww = Blocks.WHITE_WOOL;
 		RECIPES.addShaped(ModRegistry.BAGS.get(EnumDyeColor.WHITE), 3, 3, ww, Items.STRING, ww, ww, Blocks.ENDER_CHEST, ww, ww, ww, ww);
+	}
+
+	@SubscribeEvent
+	public void onPickup(EntityItemPickupEvent e) {
+		if (e.getItem().getItem().getItem() == Blocks.ENDER_CHEST.asItem()) {
+			EntityPlayer player = e.getEntityPlayer();
+			player.unlockRecipes(RECIPES.getRecipes());
+		}
+	}
+
+	@SubscribeEvent
+	public void craft(ItemCraftedEvent e) {
+		if (e.getCrafting().getItem() == Blocks.ENDER_CHEST.asItem()) {
+			EntityPlayer player = e.getPlayer();
+			player.unlockRecipes(RECIPES.getRecipes());
+		}
 	}
 }
