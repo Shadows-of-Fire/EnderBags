375a99eb1c03ede4e999c350dab1f6bb10e15931
Shadows_of_Fire <Bward7864@gmail.com>
2019-03-19 18:11:17 -0400

    Closes #1

--- src/main/java/gigabit101/EnderBags/gui/GuiEnderBag.java
+++ src/main/java/gigabit101/EnderBags/gui/GuiEnderBag.java
@@ -4,6 +4,7 @@ import gigabit101.EnderBags.EnderBags;
 import gigabit101.EnderBags.container.ContainerEnderBag;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 
@@ -32,6 +33,7 @@ public class GuiEnderBag extends GuiContainer {
 		this.mc.getTextureManager().bindTexture(texture);
 		int k = (this.width - this.xSize) / 2;
 		int l = (this.height - this.ySize) / 2;
+		GlStateManager.color3f(1, 1, 1);
 		this.drawTexturedModalRect(k, l, 0, 0, this.xSize, this.ySize);
 	}
 
