2329bfab7ecfb7606b53c929e06cbca604b5ef49
Shadows_of_Fire <Bward7864@gmail.com>
2019-06-23 22:08:48 -0400

    Update to forge 63

--- src/main/java/gigabit101/EnderBags/ClientHandler.java
+++ src/main/java/gigabit101/EnderBags/ClientHandler.java
@@ -3,13 +3,12 @@ package gigabit101.EnderBags;
 import gigabit101.EnderBags.gui.GuiEnderBag;
 import gigabit101.EnderBags.init.ModRegistry;
 import gigabit101.EnderBags.items.IColorable;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.ScreenManager;
 import net.minecraft.client.renderer.color.IItemColor;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.ColorHandlerEvent;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
@@ -21,11 +20,7 @@ public class ClientHandler {
 	@SubscribeEvent
 	public static void clientSetup(FMLClientSetupEvent e) {
 		ScreenManager.registerFactory(ModRegistry.ENDERBAG, GuiEnderBag::new);
-		MinecraftForge.EVENT_BUS.addListener(ClientHandler::registerColors);
-	}
-
-	public static void registerColors(ColorHandlerEvent.Item e) {
-		e.getItemColors().register(new IItemColor() {
+		Minecraft.getInstance().getItemColors().register(new IItemColor() {
 			@Override
 			public int getColor(ItemStack stack, int tintIndex) {
 				return ((IColorable) stack.getItem()).getColor(stack, tintIndex);
